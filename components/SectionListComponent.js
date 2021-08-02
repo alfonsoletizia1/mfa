b75170a5423ca3b28cc4b6e2637c49451a8605c4
alfonsoletizia1 <alfonso.letizia1@studenti.unicampania.it>
2021-08-02 21:46:46 +0200

    sectionListComponent done

--- components/SectionListComponent.js
+++ components/SectionListComponent.js
@@ -8,17 +8,18 @@ import {
   SectionList,
   StatusBar,
 } from "react-native";
+import { useSelector } from "react-redux";
 
 const transformData = (team) => {
   console.log("transform data ---> team --_>", team);
   const portieri = team.filter((el) => el.R === "P");
   var sumPortieri = 0;
   portieri.forEach((element) => {
-    sumPortieri += element.value;
+    sumPortieri += Number(element.value);
   });
   return [
     {
-      role: "Portieri",
+      role: "Portieri: ",
       data: portieri,
       total: sumPortieri,
     },
@@ -56,22 +57,13 @@ const Item = ({ item }) => (
     <Text style={styles.title}>{item.value}</Text>
   </View>
 );
+const Header = ({ leftTitle, rightTitle }) => (
+  <View style={styles.headerContainer}>
+    <Text style={styles.header}>{leftTitle}</Text>
+    <Text style={styles.header}>{rightTitle}</Text>
+  </View>
+);
 export default class SectionListComponent extends Component {
-  componentDidMount() {
-    console.log("MOUNTED");
-    const { team } = this.props;
-    this.setState({
-      data: transformData(team),
-    });
-  }
-  componentDidUpdate() {
-    console.log("Update");
-    const { team } = this.props;
-    // this.setState({
-    //   data: transformData(team),
-    // });
-  }
-
   constructor(props) {
     super(props);
     const { team } = this.props;
@@ -85,17 +77,20 @@ export default class SectionListComponent extends Component {
     const { teamName, team } = this.props;
     return (
       <View style={styles.container}>
-        <Text style={styles.teamName}>{teamName}</Text>
+        <View style={styles.teamNameContainer}>
+          <Text style={styles.teamName}>{teamName}</Text>
+        </View>
         <SectionList
           sections={transformData(team)}
           keyExtractor={(item) => item.Id}
           renderItem={({ item }) => <Item item={item} />}
-          renderSectionHeader={({ section: { role } }) => (
-            <Text style={styles.header}>{role}</Text>
+          renderSectionHeader={({ section: { role, total } }) => (
+            // <Text style={styles.header}>{role}</Text>
+            <Header rightTitle={total} leftTitle={role} />
           )}
-          renderSectionFooter={({ section: { total } }) => (
-            <SectionSeparator totalValue={total} />
-          )}
+          // renderSectionFooter={({ section: { total } }) => (
+          //   <SectionSeparator totalValue={total} />
+          // )}
         />
       </View>
     );
@@ -103,12 +98,22 @@ export default class SectionListComponent extends Component {
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     padding: 10,
     borderRadius: 10,
-    margin: 10,
+    margin: 5,
     borderWidth: 1,
-    minWidth: 150,
+    // minWidth: 150,
+  },
+  headerContainer: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    padding: 3,
+    marginRight: 5,
+    margin: 2,
+    borderWidth: 1,
+    borderRadius: 5,
   },
   item: {
     flex: 1,
@@ -116,7 +121,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     backgroundColor: "#fff",
     margin: 5,
-    padding: 10,
+    padding: 5,
     marginVertical: 5,
     borderRadius: 10,
     borderColor: "black",
@@ -124,17 +129,22 @@ const styles = StyleSheet.create({
   },
   header: {
     fontSize: 16,
-    backgroundColor: "#fff",
+    // backgroundColor: "#fff",
     fontWeight: "bold",
+  },
+  teamNameContainer: {
+    padding: 5,
+    margin: 5,
+    backgroundColor: "#1be0da",
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "#1be0da",
   },
   teamName: {
     fontSize: 16,
+    color: "#fff",
     fontWeight: "bold",
     textAlign: "center",
-    borderWidth: 1,
-    borderColor: "blue",
-    borderRadius: 10,
-    borderStyle: "dashed",
   },
   title: {
     fontSize: 14,
